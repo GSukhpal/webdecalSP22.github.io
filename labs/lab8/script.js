af864507740ac40ec7b430dd2b217c49f3a6a5ee
GSukhpal <111666066+GSukhpal@users.noreply.github.com>
2022-11-15 22:17:29 -0800

    lab8 Done

---
+++ labs/lab8/script.js
@@ -0,0 +1,7 @@
+function toggleTheme() {
+    var body = document.querySelector("body");
+    body.classList.toggle("dark-mode");
+}
+var buttonVariable = document.getElementById("toggleButton");
+buttonVariable.onclick = toggleTheme;
+
